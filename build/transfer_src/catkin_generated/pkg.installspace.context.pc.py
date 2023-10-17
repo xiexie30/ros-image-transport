d887db64af77fae2cd7897fab656bbbc6b59d72a
Xie <63402426+xiexie30@users.noreply.github.com>
2023-10-17 21:18:02 +0800

    Delete build directory

--- build/transfer_src/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "transfer_src"
-PROJECT_SPACE_DIR = "/home/xjb/test_communicate/install"
-PROJECT_VERSION = "0.0.0"
